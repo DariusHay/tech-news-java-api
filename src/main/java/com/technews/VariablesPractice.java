8ab8a1674048d2dc432a9eee27289bd17090e1d1
Darius Hay <dariushay@gmail.com>
2021-08-06 11:35:47 -0400

    User model created

---
+++ src/main/java/com/technews/VariablesPractice.java
@@ -0,0 +1,15 @@
+package com.technews;
+
+public class VariablesPractice {
+    public static void main(String[] args) {
+        int num1 = 3;
+        double num2 = 4.5;
+        double sum = num1 + num2;
+        String word = "dog";
+
+        String concat = sum + word;
+
+        System.out.println(sum);
+        System.out.println(concat);
+    }
+}
